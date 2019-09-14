725a4f20ea92919577edf1c28a0b244a6ef6557b
Hendrik Ranocha <ranocha@users.noreply.github.com>
2019-09-14 07:53:00 +0300

    added Butcher product of trees (#7)

--- test/runtests.jl
+++ test/runtests.jl
@@ -27,122 +27,147 @@ for (t1,t2,t3,t4) in (trees_array,)
 end
 
 
-t = rootedtree([1])
-@test order(t) == 1
-@test σ(t) == 1
-@test γ(t) == 1
-@test α(t) == 1
-@test β(t) == α(t)*γ(t)
+# see Table 301(I) etc. in butcher2016numerical
+t1 = rootedtree([1])
+@test order(t1) == 1
+@test σ(t1) == 1
+@test γ(t1) == 1
+@test α(t1) == 1
+@test β(t1) == α(t1)*γ(t1)
 
-t = rootedtree([1, 2])
-@test order(t) == 2
-@test σ(t) == 1
-@test γ(t) == 2
-@test α(t) == 1
-@test β(t) == α(t)*γ(t)
+@inferred order(t1)
+@inferred σ(t1)
+@inferred γ(t1)
+@inferred α(t1)
+@inferred β(t1)
+@inferred t1 ∘ t1
 
-t = rootedtree([1, 2, 2])
-@test order(t) == 3
-@test σ(t) == 2
-@test γ(t) == 3
-@test α(t) == 1
-@test β(t) == α(t)*γ(t)
+t2 = rootedtree([1, 2])
+@test order(t2) == 2
+@test σ(t2) == 1
+@test γ(t2) == 2
+@test α(t2) == 1
+@test β(t2) == α(t2)*γ(t2)
+@test t2 == t1 ∘ t1
 
-t = rootedtree([1, 2, 3])
-@test order(t) == 3
-@test σ(t) == 1
-@test γ(t) == 6
-@test α(t) == 1
-@test β(t) == α(t)*γ(t)
+t3 = rootedtree([1, 2, 2])
+@test order(t3) == 3
+@test σ(t3) == 2
+@test γ(t3) == 3
+@test α(t3) == 1
+@test β(t3) == α(t3)*γ(t3)
+@test t3 == t2 ∘ t1
 
-t = rootedtree([1, 2, 2, 2])
-@test order(t) == 4
-@test σ(t) == 6
-@test γ(t) == 4
-@test α(t) == 1
-@test β(t) == α(t)*γ(t)
+t4 = rootedtree([1, 2, 3])
+@test order(t4) == 3
+@test σ(t4) == 1
+@test γ(t4) == 6
+@test α(t4) == 1
+@test β(t4) == α(t4)*γ(t4)
+@test t4 == t1 ∘ t2
 
-t = rootedtree([1, 2, 2, 3])
-@inferred RootedTrees.subtrees(t)
-@test order(t) == 4
-@test σ(t) == 1
-@test γ(t) == 8
-@test α(t) == 3
-@test β(t) == α(t)*γ(t)
+t5 = rootedtree([1, 2, 2, 2])
+@test order(t5) == 4
+@test σ(t5) == 6
+@test γ(t5) == 4
+@test α(t5) == 1
+@test β(t5) == α(t5)*γ(t5)
+@test t5 == t3 ∘ t1
 
-t = rootedtree([1, 2, 3, 3])
-@test order(t) == 4
-@test σ(t) == 2
-@test γ(t) == 12
-@test β(t) == α(t)*γ(t)
+t6 = rootedtree([1, 2, 2, 3])
+@inferred RootedTrees.subtrees(t6)
+@test order(t6) == 4
+@test σ(t6) == 1
+@test γ(t6) == 8
+@test α(t6) == 3
+@test β(t6) == α(t6)*γ(t6)
+@test t6 == t2 ∘ t2 == t4 ∘ t1
 
-t = rootedtree([1, 2, 3, 4])
-@test order(t) == 4
-@test σ(t) == 1
-@test γ(t) == 24
-@test α(t) == 1
+t7 = rootedtree([1, 2, 3, 3])
+@test order(t7) == 4
+@test σ(t7) == 2
+@test γ(t7) == 12
+@test β(t7) == α(t7)*γ(t7)
+@test t7 == t1 ∘ t3
 
-t = rootedtree([1, 2, 2, 2, 2])
-@test order(t) == 5
-@test σ(t) == 24
-@test γ(t) == 5
-@test α(t) == 1
-@test β(t) == α(t)*γ(t)
+t8 = rootedtree([1, 2, 3, 4])
+@test order(t8) == 4
+@test σ(t8) == 1
+@test γ(t8) == 24
+@test α(t8) == 1
+@test t8 == t1 ∘ t4
 
-t = rootedtree([1, 2, 2, 2, 3])
-@test order(t) == 5
-@test σ(t) == 2
-@test γ(t) == 10
-@test α(t) == 6
-@test β(t) == α(t)*γ(t)
+t9 = rootedtree([1, 2, 2, 2, 2])
+@test order(t9) == 5
+@test σ(t9) == 24
+@test γ(t9) == 5
+@test α(t9) == 1
+@test β(t9) == α(t9)*γ(t9)
+@test t9 == t5 ∘ t1
 
-t = rootedtree([1, 2, 2, 3, 3])
-@test order(t) == 5
-@test σ(t) == 2
-@test γ(t) == 15
-@test α(t) == 4
+t10 = rootedtree([1, 2, 2, 2, 3])
+@test order(t10) == 5
+@test σ(t10) == 2
+@test γ(t10) == 10
+@test α(t10) == 6
+@test β(t10) == α(t10)*γ(t10)
+@test t10 == t3 ∘ t2 == t6 ∘ t1
 
-t = rootedtree([1, 2, 2, 3, 4])
-@test order(t) == 5
-@test σ(t) == 1
-@test γ(t) == 30
-@test α(t) == 4
-@test β(t) == α(t)*γ(t)
+t11 = rootedtree([1, 2, 2, 3, 3])
+@test order(t11) == 5
+@test σ(t11) == 2
+@test γ(t11) == 15
+@test α(t11) == 4
+@test t11 == t2 ∘ t3 == t7 ∘ t1
 
-t = rootedtree([1, 2, 3, 2, 3])
-@test order(t) == 5
-@test σ(t) == 2
-@test γ(t) == 20
-@test α(t) == 3
-@test β(t) == α(t)*γ(t)
+t12 = rootedtree([1, 2, 2, 3, 4])
+@test order(t12) == 5
+@test σ(t12) == 1
+@test γ(t12) == 30
+@test α(t12) == 4
+@test β(t12) == α(t12)*γ(t12)
+@test t12 == t2 ∘ t4 == t8 ∘ t1
 
-t = rootedtree([1, 2, 3, 3, 3])
-@test order(t) == 5
-@test σ(t) == 6
-@test γ(t) == 20
-@test α(t) == 1
-@test β(t) == α(t)*γ(t)
+t13 = rootedtree([1, 2, 3, 2, 3])
+@test order(t13) == 5
+@test σ(t13) == 2
+@test γ(t13) == 20
+@test α(t13) == 3
+@test β(t13) == α(t13)*γ(t13)
+@test t13 == t4 ∘ t2
 
-t = rootedtree([1, 2, 3, 3, 4])
-@test order(t) == 5
-@test σ(t) == 1
-@test γ(t) == 40
-@test α(t) == 3
-@test β(t) == α(t)*γ(t)
+t14 = rootedtree([1, 2, 3, 3, 3])
+@test order(t14) == 5
+@test σ(t14) == 6
+@test γ(t14) == 20
+@test α(t14) == 1
+@test β(t14) == α(t14)*γ(t14)
+@test t14 == t1 ∘ t5
 
-t = rootedtree([1, 2, 3, 4, 4])
-@test order(t) == 5
-@test σ(t) == 2
-@test γ(t) == 60
-@test α(t) == 1
-@test β(t) == α(t)*γ(t)
+t15 = rootedtree([1, 2, 3, 3, 4])
+@test order(t15) == 5
+@test σ(t15) == 1
+@test γ(t15) == 40
+@test α(t15) == 3
+@test β(t15) == α(t15)*γ(t15)
+@test t15 == t1 ∘ t6
 
-t = rootedtree([1, 2, 3, 4, 5])
-@test order(t) == 5
-@test σ(t) == 1
-@test γ(t) == 120
-@test α(t) == 1
-@test β(t) == α(t)*γ(t)
+t16 = rootedtree([1, 2, 3, 4, 4])
+@test order(t16) == 5
+@test σ(t16) == 2
+@test γ(t16) == 60
+@test α(t16) == 1
+@test β(t16) == α(t16)*γ(t16)
+@test t16 == t1 ∘ t7
+
+t17 = rootedtree([1, 2, 3, 4, 5])
+@test order(t17) == 5
+@test σ(t17) == 1
+@test γ(t17) == 120
+@test α(t17) == 1
+@test β(t17) == α(t17)*γ(t17)
+@test t17 == t1 ∘ t8
+
 
 # see butcher2008numerical, Table 302(I)
 number_of_rooted_trees = [1, 1, 2, 4, 9, 20, 48, 115, 286, 719]
