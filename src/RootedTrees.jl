3bdf45deac48f95b7d9e30eb33e249d1ac2daa82
Hendrik Ranocha <mail@ranocha.de>
2021-09-28 16:56:23 +0200

    implement RootedTrees.latexify

--- src/RootedTrees.jl
+++ src/RootedTrees.jl
@@ -1039,7 +1039,7 @@ function butcher_representation(t::RootedTree, normalize::Bool=true)
   subtr = Subtrees(t)
   result = ""
   for i in eachindex(subtr)
-    result = result * butcher_representation(subtr[i])
+    result = result * butcher_representation(subtr[i], normalize)
   end
   result = "[" * result * "]"
 
@@ -1066,6 +1066,71 @@ function butcher_representation(t::RootedTree, normalize::Bool=true)
   end
 
   return result
+end
+
+
+"""
+    latexify(t::RootedTree)
+
+Return a LaTeX representation of the rooted tree `t`. This makes use of the
+LaTeX package [forest](https://ctan.org/pkg/forest) and assumes that you use
+the following LaTeX code in the preamble.
+
+```
+% Butcher trees, cf. https://tex.stackexchange.com/questions/283343/butcher-trees-in-tikz
+\\usepackage{forest}
+\\forestset{
+  */.style={
+    delay+={append={[]},}
+  },
+  rooted tree/.style={
+    for tree={
+      grow'=90,
+      parent anchor=center,
+      child anchor=center,
+      s sep=2.5pt,
+      if level=0{
+        baseline
+      }{},
+      delay={
+        if content={*}{
+          content=,
+          append={[]}
+        }{}
+      }
+    },
+    before typesetting nodes={
+      for tree={
+        circle,
+        fill,
+        minimum width=3pt,
+        inner sep=0pt,
+        child anchor=center,
+      },
+    },
+    before computing xy={
+      for tree={
+        l=5pt,
+      }
+    }
+  }
+}
+\\DeclareDocumentCommand\\rootedtree{o}{\\Forest{rooted tree [#1]}}
+```
+
+# Examples
+
+```jldoctest
+julia> rootedtree([1, 2, 2]) |> RootedTrees.latexify |> println
+\rootedtree[[][]]
+
+julia> rootedtree([1, 2, 3, 3, 2]) |> RootedTrees.latexify |> println
+\rootedtree[[[][]][]]
+```
+"""
+function latexify(t::RootedTree)
+  list_representation = butcher_representation(t, false)
+  "\\rootedtree" * replace(list_representation, "τ" => "[]")
 end
 
 
