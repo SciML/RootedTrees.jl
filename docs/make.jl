94d88ed7d5aaf9759d4072a4f4b0a224e60a63e8
Hendrik Ranocha <ranocha@users.noreply.github.com>
2023-11-17 10:34:33 +0100

    allow building the docs locally (#143)

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,13 @@
 using Documenter
 import Pkg
+
+# Fix for https://github.com/trixi-framework/Trixi.jl/issues/668
+# to allow building the docs locally
+if (get(ENV, "CI", nothing) != "true") &&
+   (get(ENV, "JULIA_DOC_DEFAULT_ENVIRONMENT", nothing) != "true")
+    push!(LOAD_PATH, dirname(@__DIR__))
+end
+
 using RootedTrees
 
 # Define module-wide setups such that the respective modules are available in doctests
