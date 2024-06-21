b2bd78d2ea905cc4e27f41d5897a980c778d12f9
Hendrik Ranocha <ranocha@users.noreply.github.com>
2024-06-21 21:13:06 +0200

    fix typo

--- src/RootedTrees.jl
+++ src/RootedTrees.jl
@@ -1364,7 +1364,7 @@ Compute the non-associative Butcher product `t = t1 ∘ t2` of rooted trees
 in-place. It is formed by adding an edge from the root of `t1` to the root
 of `t2`.
 
-See also [`∘`](@ref) (available as `\circ` plus TAB).
+See also [`∘`](@ref) (available as `\\circ` plus TAB).
 
 Reference: Section 301 of
 - Butcher, John Charles.
