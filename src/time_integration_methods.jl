239d2435b06ae0ccac050ea57cd4bf167b1d6d13
Hendrik Ranocha <ranocha@users.noreply.github.com>
2025-11-17 10:18:10 +0100

    Fix docstring of elementary_weight

--- src/time_integration_methods.jl
+++ src/time_integration_methods.jl
@@ -52,7 +52,7 @@ end
     elementary_weight(t::RootedTree, A::AbstractMatrix, b::AbstractVector, c::AbstractVector)
 
 Compute the elementary weight Φ(`t`) of the [`RungeKuttaMethod`](@ref) `rk`
-with Butcher coefficients `A, b, c` for a rooted tree `t``.
+with Butcher coefficients `A, b, c` for a rooted tree `t`.
 
 Reference: Section 312 of
 - Butcher, John Charles.
